8907062551b094d5bc4c4e36d9fe4e7a6d55b911
Anurag <iamanuragmahendru@gmail.com>
2024-01-28 17:34:07 +0530

    Initial commit

---
+++ snippet.js
@@ -0,0 +1,14 @@
+const arr = [1,2,3,4];
+
+arr.forEach(item => {
+    console.log(item);
+});
+
+for (let idx = 0; idx < arr.length; idx++) {
+    const element = arr[idx];
+    console.log(element);
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+}
